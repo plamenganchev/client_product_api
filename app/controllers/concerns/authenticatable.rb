8bdf37a3f35c43dd217d8f37b5f9ac1ba743576e
plamen <plamen@DESKTOP-D7AJ6LO.localdomain>
2024-07-01 22:16:53 +0300

    track user authentication activities

--- app/controllers/concerns/authenticatable.rb
+++ app/controllers/concerns/authenticatable.rb
@@ -11,10 +11,23 @@ module Authenticatable
       decoded_token = JwtService.decode(token)
       @current_user = User.find_by(id: decoded_token[:user_id]) if decoded_token
     end
+    @current_user ? log_api_request({user: @current_user}) : log_api_request({api_key: token})
     render json: { error: 'Unauthorized' }, status: :unauthorized unless @current_user
   end
 
   def current_user
     @current_user
+  end
+
+  def log_api_request(additional_params)
+    begin
+      ApiRequest.create({
+        endpoint: request.fullpath,
+        remote_ip: request.remote_ip,
+        payload: params}.merge(additional_params)
+      )
+    rescue StandardError => e
+      #Maybe log this in something like sentry
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_30_173223) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_01_185537) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "api_requests", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "api_key"
+    t.string "endpoint"
+    t.string "remote_ip"
+    t.string "status"
+    t.json "payload"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "brands", force: :cascade do |t|
     t.string "name"
